5b43a18e3686336ec512ada63e4a767dc136fd66
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-05-23 23:10:44 -0300

    Finish Change Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
+import { ThemeProvider} from 'styled-components';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 function App() {
+
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }
+
   return (
-    <div className="App">
-    
+   <ThemeProvider theme={theme}>
+      <div className="App">
+      <GlobalStyle/>
+      <Header toggleTheme={toggleTheme} />
     </div>
+   </ThemeProvider>
   );
 }
 
